1338ccd8f33f7910184c4bbf5365b0e47230b9dd
Andre Juan <andrjetx@yahoo.com.br>
2024-10-16 20:34:58 -0300

    Feat: adicionando os repositorios pela api

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,20 +3,53 @@ document.getElementById('btn-search').addEventListener("click", () =>{
     getUserProfile(userName)
 })
 
-async function user() {
-    const response = await fetch ('https://api.github.com/users/AndreJetx')
+document.getElementById('input-search').addEventListener('keyup', (e) => {
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if(isEnterKeyPressed){
+        getUserProfile(userName)
+    }
+})
+
+async function user(userName) {
+    const response = await fetch (`https://api.github.com/users/${userName}`)
     return await response.json()
 }
 
-function getUserProfile(){
-    user().then( userData => {
-        let userInfo = `<img src="${userData.avatar_url}" alt="Foto do perfil do usuário" />
+async function repos(userName) {
+    const response = await fetch (`https://api.github.com/users/${userName}/repos`)
+    return await response.json()
+}
+
+function getUserProfile(userName){
+
+    user(userName).then( userData => {
+        let userInfo = `<div class="info">
+                        <img src="${userData.avatar_url}" alt="Foto do perfil do usuário" />
                         <div class="data"
                             <h1>${userData.name ?? 'Nao possui nome cadastrado 😢' }</h1>
                             <p>${userData.bio ?? 'Nao possui nome cadastrado 😢'}</p>
+                        </div>
                         </div>`
 
         document.querySelector('.profile-data').innerHTML = userInfo
+
+        getUserRepositories(userName)
     })
 }
 
+function getUserRepositories(userName){
+    repos(userName).then(reposData => {
+        let repositoriesItens = ""
+        
+        reposData.forEach(repo => {
+            repositoriesItens +=`<li><a href="${repo.html_url}" target="_blank">${repo.name}</li>`
+        })
+        document.querySelector('.profile-data').innerHTML += `<div class="repositories section">
+                                                                <h2>Repositorios</h2>
+                                                                <ul>${repositoriesItens}</ul>
+                                                            </div>`
+    })
+}
